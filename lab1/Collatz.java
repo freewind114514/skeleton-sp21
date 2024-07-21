89dea61e5414a705cd7e47a24f13cb971cc2d39e
freewind114514 <101422013+freewind114514@users.noreply.github.com>
2024-07-21 15:45:12 +0900

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -2,20 +2,19 @@
  *  @author YOUR NAME HERE
  */
 public class Collatz {
-
-    /** Buggy implementation of nextNumber! */
+    /**
+     * Calculates the next number in the Collatz sequence for a given input number.
+     * The Collatz sequence is defined as follows:
+     *  - If n is even, the next number is n / 2.
+     *  - If n is odd, the next number is 3n + 1.
+     */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
-        }
+        return n % 2 == 0 ? n / 2 : 3 * n + 1;
     }
 
     public static void main(String[] args) {
-        int n = 5;
+        int n = 5; // Starting number for the Collatz sequence
+        // Print the Collatz sequence starting from n
         System.out.print(n + " ");
         while (n != 1) {
             n = nextNumber(n);
