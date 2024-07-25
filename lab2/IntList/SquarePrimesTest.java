97c53bcd7ad2b29477ca69faacfcd0cd8669869c
freewind114514 <101422013+freewind114514@users.noreply.github.com>
2024-07-26 02:06:18 +0900

    Update SquarePrimesTest.java

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,12 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void test_always_primes() {
+        IntList lst = IntList.of(97, 17, 73, 31, 47);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("9409 -> 289 -> 5329 -> 961 -> 2209", lst.toString());
+        assertTrue(changed);
+    }
 }
