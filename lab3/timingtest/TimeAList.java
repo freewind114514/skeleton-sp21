e6b0b72d92faac1e04505d8a35b1b5ef525bbe72
freewind114514 <101422013+freewind114514@users.noreply.github.com>
2024-07-29 02:15:25 +0900

    lab 3

--- lab3/timingtest/TimeAList.java
+++ lab3/timingtest/TimeAList.java
@@ -23,5 +23,51 @@ public class TimeAList {
 
     public static void timeAListConstruction() {
         // TODO: YOUR CODE HERE
+        AList<Integer> ops = new AList<>();
+        ops.addLast(1000);
+        ops.addLast(2000);
+        ops.addLast(4000);
+        ops.addLast(8000);
+        ops.addLast(16000);
+        ops.addLast(32000);
+        ops.addLast(64000);
+        ops.addLast(128000);
+        printTimingTable(ops, times(ops), ops);
     }
+
+    /** do addLast t times and return the size N of the data structure*/
+    private static void do_addLast(int t){
+        int i = 0;
+        AList<Integer> ops = new AList<>();
+        do{
+            ops.addLast(1);
+            i++;
+        }while (i < t);
+    }
+
+    /** return the second column
+     *  (the times required to complete all operations)*/
+    private static AList<Double> times(AList<Integer> ops){
+        AList<Double> t = new AList<>();
+        for (int i = 0; i < ops.size(); i++){
+            Stopwatch sw = new Stopwatch();
+            do_addLast(ops.get(i));
+            double timeInSeconds = sw.elapsedTime();
+            t.addLast(timeInSeconds);
+        }
+        return t;
+    }
+
+    /** return first column
+     *  (size of the data structure)
+
+    private static AList<Integer> Ns(AList<Integer> ops){
+        AList<Integer> n = new AList<>();
+        for (int i = 0; i < ops.size(); i++){
+            n.addLast(do_addLast(ops.get(i)));
+        }
+        return n;
+    }
+     */
+
 }
--- lab3/timingtest/TimeSLList.java
+++ lab3/timingtest/TimeSLList.java
@@ -23,6 +23,63 @@ public class TimeSLList {
 
     public static void timeGetLast() {
         // TODO: YOUR CODE HERE
+        AList<SLList<Integer>> N = new AList<>();
+        N.addLast(do_addLast(1000));
+        N.addLast(do_addLast(2000));
+        N.addLast(do_addLast(4000));
+        N.addLast(do_addLast(8000));
+        N.addLast(do_addLast(16000));
+        N.addLast(do_addLast(32000));
+        N.addLast(do_addLast(64000));
+        N.addLast(do_addLast(128000));
+        int i = 0;
+        AList<Integer> ops = new AList<>();
+        do{
+            ops.addLast(10000);
+            i++;
+        }while (i < N.size());
+        AList<Integer> ns = new AList<>();
+        ns.addLast(1000);
+        ns.addLast(2000);
+        ns.addLast(4000);
+        ns.addLast(8000);
+        ns.addLast(16000);
+        ns.addLast(32000);
+        ns.addLast(64000);
+        ns.addLast(128000);
+        printTimingTable(ns, times(N), ops);
+    }
+
+    /** do addLast t times and return a SLList*/
+    private static SLList<Integer> do_addLast(int t){
+        int i = 0;
+        SLList<Integer> N = new SLList<>();
+        do{
+            N.addLast(1);
+            i++;
+        }while (i < t);
+        return N;
+    }
+
+    private static void do_getLast(SLList<Integer> N, int t){
+        int i = 0;
+        do{
+            N.getLast();
+            i++;
+        }while (i < t);
+    }
+
+
+
+    private static AList<Double> times(AList<SLList<Integer>> N){
+        AList<Double> t = new AList<>();
+        for (int i = 0; i < N.size(); i++){
+            Stopwatch sw = new Stopwatch();
+            do_getLast(N.get(i),10000);
+            double timeInSeconds = sw.elapsedTime();
+            t.addLast(timeInSeconds);
+        }
+        return t;
     }
 
 }
