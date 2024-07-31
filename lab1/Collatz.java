68aa3f518406e5a8a2e2b3fa367c4f590ebc0ccb
freewind114514 <101422013+freewind114514@users.noreply.github.com>
2024-08-01 02:57:46 +0900

    format lab1

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,18 +1,20 @@
-/** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+/**
+ * Class that prints the Collatz sequence starting from a given number.
+ *
+ * @author YOUR NAME HERE
  */
 public class Collatz {
     /**
      * Calculates the next number in the Collatz sequence for a given input number.
      * The Collatz sequence is defined as follows:
-     *  - If n is even, the next number is n / 2.
-     *  - If n is odd, the next number is 3n + 1.
+     * - If n is even, the next number is n / 2.
+     * - If n is odd, the next number is 3n + 1.
      */
     public static int nextNumber(int n) {
-        if(n%2==0){
-            return n/2;
-        }else{
-            return 3*n+1;
+        if (n % 2 == 0) {
+            return n / 2;
+        } else {
+            return 3 * n + 1;
         }
     }
 
